ef7d95f7fc91bb29ad42431cf5e500add2362d24
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2023-07-06 18:38:44 -0400

    fff

--- useCases/alertCases/saveAlertWithSender/saveAlertWithSender.js
+++ useCases/alertCases/saveAlertWithSender/saveAlertWithSender.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 import getDateChile from "../../../utils/getDateChile.js";
 import saveOne from "../../../repositories/saveOne/saveOne.js";
 
-var dateOptions = { timeZone: "UTC", weekday: 'short', year: 'numeric', month: '2-day', day: 'numeric' };
-
 const saveAlertWithSender = async (req,res) => {
   const db = req.db;
   const {senderId} = req.body;
-  const alert = await saveOne(db.Alert, {sender: new mongoose.Types.ObjectId(senderId), date: new Date().toLocaleString( "es-CL", dateOptions) })
+  const alert = await saveOne(db.Alert, {sender: new mongoose.Types.ObjectId(senderId), date: new Date().toLocaleString( "es-CL", {timeZone: "UTC"}).toUTCString() })
   res.json(alert);
 }
 
