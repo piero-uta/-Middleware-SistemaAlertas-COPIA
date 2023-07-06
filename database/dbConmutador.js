18a7ff854d1f72c7e8ee481afffee8a8764493a7
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-07-06 18:49:01 -0400

    update

--- database/dbConmutador.js
+++ database/dbConmutador.js
@@ -4,26 +4,14 @@ import dbData from './dbData.js';
 
 class dbConmutador {
   constructor(){
-    this.data = dbData;
+    this.data = new dbData();
     this.inicializar();
   }
   inicializar(){
     this.data.primaria.on('connected', () => {this.usePrimaria();});
     this.data.primaria.on('disconnected', () => {this.useSecundaria();});
     this.data.secundaria.on('connected', () => {this.data.secundariaReady = true;});
-    this.data.secundaria.on('disconnected', () => {this.data.secundariaReady = false; if (!this.data.primariaReady) {
-      throw new Error('Any Database connected',getDateChile());}
-    });  
-    cron.schedule('00 * * * * *',()=>{
-      if (this.data.primariaReady && this.data.secundariaReady){
-        Promise.all([userModels[dbMap.secundaria].deleteMany({}),tokenModels[dbMap.secundaria].deleteMany({}),alertModels[dbMap.secundaria].deleteMany({})])
-        Promise.all([copyUsersModelAtoModelB(userModels[dbMap.primaria],userModels[dbMap.secundaria]),copyTokensModelAtoModelB(tokenModels[dbMap.primaria],tokenModels[dbMap.secundaria])],copyAlertsModelAtoModelB(alertModels[dbMap.primaria],alertModels[dbMap.secundaria]));
-        console.log('copy primaria to secundaria', getDateChile());
-        }
-    }, {
-      scheduled: true,
-      timezone: "America/Santiago"
-    });  
+    this.data.secundaria.on('disconnected', () => {this.data.secundariaReady = false; this.data.verifyDbsReady();});  
   }
   usePrimaria(){
     if (this.data.secundariaReady){
@@ -31,24 +19,12 @@ class dbConmutador {
       Promise.all([copyUsersModelAtoModelB(userModels[dbMap.secundaria],userModels[dbMap.primaria]),copyTokensModelAtoModelB(tokenModels[dbMap.secundaria],tokenModels[dbMap.primaria]),copyAlertsModelAtoModelB(alertModels[dbMap.secundaria],alertModels[dbMap.primaria])])
       console.log("copy secundaria to primaria",getDateChile());
     }
-    console.log('use primaria', getDateChile());
-    this.data.User = userModels[dbMap.primaria];
-    this.data.Token = tokenModels[dbMap.primaria];
-    this.data.Alert = alertModels[dbMap.primaria];
-    
+    this.data.useModelsPrimaria(dbMap.primaria);    
     this.data.primariaReady = true;      
   }
   useSecundaria(){
-    if(!this.data.secundariaReady){
-      throw new Error('Any Database connected', getDateChile());
-    }else{
-      console.log('use secundaria', getDateChile());
-  
-      this.data.User = userModels[dbMap.secundaria];
-      this.data.Token = tokenModels[dbMap.secundaria];
-      this.data.Alert = alertModels[dbMap.secundaria];
-  
-    }
+    this.data.verifyDbsReady();
+    this.data.useModelsSecundaria(dbMap.secundaria);  
     this.data.primariaReady = false;  
   }
   get User(){return this.data.User;}
--- database/dbData.js
+++ database/dbData.js
@@ -1,13 +1,40 @@
-import { dbMap, conns, userModels, tokenModels, alertModels } from "./dbModules.js";
+import { dbMap, conns, userModels, tokenModels, alertModels, getDateChile } from "./dbModules.js";
 
-var dbData = {
-  primaria : conns[dbMap.primaria],
-  secundaria : conns[dbMap.secundaria],
-  primariaReady : true,
-  secundariaReady : true,
-  User : userModels[dbMap.primaria],
-  Token : tokenModels[dbMap.primaria],
-  Alert : alertModels[dbMap.primaria]
+class dbData {
+  constructor () {
+    this.primaria = conns[dbMap.primaria];
+    this.secundaria = conns[dbMap.secundaria];
+    this.primariaReady = true;
+    this.secundariaReady = true;
+    this.User = userModels[dbMap.primaria];
+    this.Token = tokenModels[dbMap.primaria];
+    this.Alert = alertModels[dbMap.primaria];  
+    this.planificadorCopias();
+  }
+  planificadorCopias() {
+    cron.schedule('00 * * * * *',()=>{
+      if (this.primariaReady && this.secundariaReady){
+        Promise.all([userModels[dbMap.secundaria].deleteMany({}),tokenModels[dbMap.secundaria].deleteMany({}),alertModels[dbMap.secundaria].deleteMany({})])
+        Promise.all([copyUsersModelAtoModelB(userModels[dbMap.primaria],userModels[dbMap.secundaria]),copyTokensModelAtoModelB(tokenModels[dbMap.primaria],tokenModels[dbMap.secundaria])],copyAlertsModelAtoModelB(alertModels[dbMap.primaria],alertModels[dbMap.secundaria]));
+        console.log('copy primaria to secundaria', getDateChile());
+        }
+    }, {
+      scheduled: true,
+      timezone: "America/Santiago"
+    });  
+  }
+  useModels(dbMapModel) {
+    console.log(`use models ${dbMapModel}`, getDateChile());
+    this.User = userModels[dbMapModel];
+    this.Token = tokenModels[dbMapModel];
+    this.Alert = alertModels[dbMapModel];
+  }
+
+  verifyDbsReady () {
+    if (!this.primariaReady && !this.secundariaReady) {
+      throw new Error('Any Database connected',getDateChile());
+    }
+  }
 }
 
 export default dbData;
