f54f0f7ee697b88ea3405dc0fd13bd3529a03706
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2023-07-07 16:28:33 -0400

    moment-timezone

--- useCases/alertCases/saveAlertWithSender/saveAlertWithSender.js
+++ useCases/alertCases/saveAlertWithSender/saveAlertWithSender.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 import getDateChile from "../../../utils/getDateChile.js";
 import saveOne from "../../../repositories/saveOne/saveOne.js";
 
+const moment = require('moment-timezone');
+const dateChile = moment.tz(Date.now(), "America/Santiago");
+
+
 const saveAlertWithSender = async (req,res) => {
   const db = req.db;
   const {senderId} = req.body;
-  const alert = await saveOne(db.Alert, {sender: new mongoose.Types.ObjectId(senderId), date: new Date().toLocaleString( "es-CL", {timeZone: "UTC"}).toUTCString() })
+  const alert = await saveOne(db.Alert, {sender: new mongoose.Types.ObjectId(senderId), date: dateChile})
   res.json(alert);
 }
 
